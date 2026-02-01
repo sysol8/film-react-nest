fa00013d58cf06081011c41bf90baef6c2830f28
sysol8 <iabrosimov@list.ru>
2026-02-01 12:58:16 +0300

    feat: initialized app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,33 @@
 import { Module } from '@nestjs/common';
-import {ServeStaticModule} from "@nestjs/serve-static";
-import {ConfigModule} from "@nestjs/config";
-import * as path from "node:path";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as path from 'node:path';
 
-import {configProvider} from "./app.config.provider";
+import { configProvider } from './app.config.provider';
+import { OrderController } from './order/order.controller';
+import { OrderService } from './order/order.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FilmsModule } from './films/films.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-	ConfigModule.forRoot({
-          isGlobal: true,
-          cache: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'public', 'content', 'afisha'),
+      serveRoot: '/content/afisha',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URL'),
       }),
-      // @todo: Добавьте раздачу статических файлов из public
+    }),
+    FilmsModule,
+    OrderModule,
   ],
   controllers: [],
   providers: [configProvider],
