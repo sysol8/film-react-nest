8b267f507aa812b7a2e6656a94606cb052ad9432
sysol8 <iabrosimov@list.ru>
2026-02-01 12:56:59 +0300

    feat: added film dtos

---
+++ backend/src/films/dto/film-list-item.dto.ts
@@ -0,0 +1,11 @@
+export class FilmListItemDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  image: string;
+  cover: string;
+  title: string;
+  about: string;
+  description: string;
+}
